4241cc7f6fb62b5979c7be4ae53160e1f60654cd
karthikctech <karthikctech>
2021-06-12 18:21:13 +0530

    Typingteat

---
+++ Python-bootcamp/Python-Advanced/TypingTest/typingspeedtest.py
@@ -0,0 +1,11 @@
+from tkinter import TK
+from tkinter import messagebox
+from timeit import default_timer as timer
+import random
+
+root = TK()
+root.geometry("500x500")
+root.config(bg="Black")
+
+
+root.mainloop()
