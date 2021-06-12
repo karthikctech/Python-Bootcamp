cc5ae7073b4ebf1f5bad4911f00c442030db15c0
karthikctech <karthikctech>
2021-06-12 10:00:32 +0530

    AmazonPriceTracked

---
+++ Python-bootcamp/Python-Advanced/AmazonPriceTracked/main.py
@@ -0,0 +1,36 @@
+import smtplib
+import requests
+import lxml
+from bs4 import BeautifulSoup
+url - "https://www.amazon.com/Xiaomi-REDMI-128GB-Interstellar-M2003J15SS/dp/B088HJ3FCX/ref=sr_1_10?dchild=1&keywords=redmi+note+8&qid=1623388849&sr=8-10"
+header = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
+    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
+}
+
+response = requests.get(url, headers=header)
+
+soup = BeautifulSoup(response.content, "lxml")
+print(soup.prettify())
+
+price = soup.find(id="priceblock_ourprice").get_text()
+price_without_currency = price.split("$")[1]
+price_as_float = float(price_without_currency)
+print(price_as_float)
+
+title = soup.find(id="productTitle").get_text().strip()
+print(title)
+
+BUY_PRICE = 200
+
+if price_as_float < BUY_PRICE:
+    message = f"{title} is now {price}"
+
+    with smtplib.SMTP(YOUR_SMTP_ADDRESS, port=587) as connection:
+        connection.starttls()
+        result = connection.login(YOUR_EMAIL, YOUR_PASSWORD)
+        connection.sendmail(
+            from_addr=YOUR_EMAIL,
+            to_addrs=YOUR_EMAIL,
+            msg=f"Subject:Amazon Price Alert!\n\n{message}\n{url}"
+        )
