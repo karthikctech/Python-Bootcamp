6eee4a2f1043266d60c27e4bf03796ca10b9a89b
karthikctech <karthikctech>
2021-06-12 11:46:39 +0530

    LoopConcepts

---
+++ Python-bootcamp/Python-Advanced/Looping/looping.py
@@ -0,0 +1,12 @@
+# for loop
+# print even number 2 to 100
+import math
+ck = int(input('enter the even number'))
+for ck in range(ck,100, 2):
+    print(ck)
+def even():
+    print('your result')
+even()
+
+
+   
