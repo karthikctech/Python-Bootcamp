8774fae01f1cfe51bcf0042aee45dc180515d647
karthikctech <karthikctech>
2021-06-12 19:42:46 +0530

    ANALYSISFLIGHTCHEPESTPRICE

---
+++ Python-bootcamp/Python-Advanced/FlightProject/flight_search.py
@@ -0,0 +1,11 @@
+import requests
+
+TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
+
+
+class FlightSearch:
+
+    def get_destination_code(self, city_name):
+        # Return "TESTING" for now to make sure Sheety is working. Get TEQUILA API data later.
+        code = "TESTING"
+        return code
