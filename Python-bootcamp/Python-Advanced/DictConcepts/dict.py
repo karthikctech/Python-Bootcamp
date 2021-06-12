025ef83a55b4ff19959dabcfacc4e71e5097bd92
karthikctech <karthikctech>
2021-06-12 18:23:59 +0530

    DICTCONCEPTS

---
+++ Python-bootcamp/Python-Advanced/DictConcepts/dict.py
@@ -0,0 +1,19 @@
+programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+
+print(programming_dictionary["Bug"])
+# changing the vALIE
+programming_dictionary["Bug"] = "you got some error"
+
+
+
+# add the item
+programming_dictionary['karthik'] = 'hhe is the game developer in future'
+
+print(programming_dictionary)
+
+# creating empty dictanories
+
+programming_dictionary = {}
+
+print(programming_dictionary)
+
